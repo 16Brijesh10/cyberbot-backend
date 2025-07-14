cc177842f0ecc7b379189ad4ca51154a88b6cf4d
Brijesh Arumuganainar <110632170+16Brijesh10@users.noreply.github.com>
2025-07-14 14:19:35 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-PDF_PATHS = [
+PDF_PATH = [
     os.path.join(BASE_DIR, "CyberTech Information Security Guidelines - Do's  Don'ts.pdf"),
     os.path.join(BASE_DIR, "CyberTech Reference Manual_CyberTech_Code of Business Conduct and Ethics_ V_04.3.pdf"),
     os.path.join(BASE_DIR, "CyberTech WorkFromHome Guidelines.pdf"),
