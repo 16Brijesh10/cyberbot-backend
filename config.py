4deeca2000e998c27734200591b485be93aba283
Brijesh Arumuganainar <110632170+16Brijesh10@users.noreply.github.com>
2025-07-14 13:41:51 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 PDF_PATHS = [
     os.path.join(BASE_DIR, "CyberTech Information Security Guidelines - Do's  Don'ts.pdf"),
     os.path.join(BASE_DIR, "CyberTech Reference Manual_CyberTech_Code of Business Conduct and Ethics_ V_04.3.pdf"),
     os.path.join(BASE_DIR, "CyberTech WorkFromHome Guidelines.pdf"),
-    os.path.join(BASE_DIR, "Joining Documents Checklist.pdf")
+    os.path.join(BASE_DIR, "Joining Documents Checklist.pdf"),
 ]
 
-DB_PATH = os.path.join(BASE_DIR, "db")
+# Use /tmp for writable db directory on Render
+DB_PATH = "/tmp/db"
