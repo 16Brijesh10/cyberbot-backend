7f30370465e7ab8967b2456064fce83dbb0f6ed9
Brijesh Arumuganainar <110632170+16Brijesh10@users.noreply.github.com>
2026-08-10 01:11:27 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PDF_PATH = [
     os.path.join(BASE_DIR, "CyberTech Information Security Guidelines - Do's  Don'ts.pdf"),
     os.path.join(BASE_DIR, "CyberTech Reference Manual_CyberTech_Code of Business Conduct and Ethics_ V_04.3.pdf"),
-    os.path.join(BASE_DIR, "CyberTech WorkFromHome Guidelines.pdf"),
+    
 ]
 
 # Use /tmp for writable db directory on Render
