4b91ffa6fcaeea4411fa573afb0f44208e806ea9
Brijesh Arumuganainar <110632170+16Brijesh10@users.noreply.github.com>
2026-08-10 15:55:09 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 PDF_PATH = [
-    os.path.join(BASE_DIR, "CyberTech Information Security Guidelines - Do's  Don'ts.pdf"),
-    os.path.join(BASE_DIR, "CyberTech Reference Manual_CyberTech_Code of Business Conduct and Ethics_ V_04.3.pdf"),
-    
+    os.path.join(BASE_DIR, "CyberTech Information Security Guidelines - Do's  Don'ts.pdf")
 ]
 
 # Use /tmp for writable db directory on Render
